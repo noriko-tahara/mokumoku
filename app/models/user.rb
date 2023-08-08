66835dc1bd81ed0ece58370bcfd1fc04ed255e96
Noriko <116807250+noriko-tahara@users.noreply.github.com>
2023-08-08 15:29:56 +0900

    他のユーザーをフォロー・アンフォローする機能を実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,10 @@ class User < ApplicationRecord
   has_many :notifications, foreign_key: :receiver_id, dependent: :destroy, inverse_of: :sender
   has_many :user_notification_timings, dependent: :destroy
   has_many :notification_timings, through: :user_notification_timings
+  has_many :relationships, dependent: :destroy
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id', dependent: :destroy
+  has_many :followers, through: :reverse_of_relationships, source: :user
   has_one_attached :avatar
 
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
@@ -79,5 +83,19 @@ class User < ApplicationRecord
 
   def to_param
     name
+  end
+
+  def follow(other_user)
+    unless self == other_user
+      relationships.find_or_create_by!(follow_id: other_user.id)
+    end
+  end
+
+  def following?(other_user)
+    followings.include?(other_user)
+  end
+
+  def unfollow(other_user)
+    relationships.find_by(follow_id: other_user.id).destroy!
   end
 end
